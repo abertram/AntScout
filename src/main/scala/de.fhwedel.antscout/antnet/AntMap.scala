6895933fc89b8c17e609292512e6d3876691eb75
Alexander Bertram <bertram.alex@gmail.com>
2011-12-27 13:59:43 +0100

    Berechnung der Knoten-Nachbarn und aller erreichbaren Knoten entfernt

--- src/main/scala/de.fhwedel.antscout/antnet/AntMap.scala
+++ src/main/scala/de.fhwedel.antscout/antnet/AntMap.scala
@@ -16,11 +16,6 @@ import scala.collection.mutable.{Map => MutableMap}
 class AntMap(val nodes: Map[Int, AntNode], val ways: Map[String, AntWay]) extends Logger {
   val incomingWays: Map[AntNode, Iterable[AntWay]] = computeIncomingWays
   val outgoingWays: Map[AntNode, Iterable[AntWay]] = computeOutgoingWays
-  /**
-   * Direkte Nachbarn eines Knoten.
-   */
-  val neighbours: Map[AntNode, Set[AntNode]] = computeNeighbours
-  // val reachableNodes: Map[AntNode, Set[AntNode]] = computeReachableNodes
 
   private def computeIncomingWays = {
     info("Computing incoming ways")  
@@ -50,63 +45,6 @@ class AntMap(val nodes: Map[Int, AntNode], val ways: Map[String, AntWay]) extend
     }.seq.toMap)
     info("Outgoing ways computed in %d ms".format(time))
     outgoingWays
-  }
-
-  /**
-   * Berechnet die direkten Nachbarn eines Knoten.
-   */
-  def computeNeighbours = {
-    def createNeighbour(startNode: AntNode, endNode: AntNode, neighbours: Map[AntNode, Set[AntNode]]): Map[AntNode, Set[AntNode]] = {
-      neighbours + (startNode -> (neighbours.getOrElse(startNode, Set(endNode)) + endNode))
-    }
-    @scala.annotation.tailrec
-    def computeNeighboursRecursive(ways: List[AntWay], neighbours: Map[AntNode, Set[AntNode]]): Map[AntNode, Set[AntNode]] = {
-      ways match {
-        case Nil =>
-          neighbours
-        case head :: tail =>
-          head match {
-            case oneWay: AntOneWay =>
-              val endNodeNeighbours = neighbours.getOrElse(oneWay.endNode, Set.empty[AntNode])
-              val newNeighbours = createNeighbour(oneWay.startNode, oneWay.endNode, neighbours + (oneWay.endNode -> endNodeNeighbours))
-              computeNeighboursRecursive(tail, newNeighbours)
-            case way: AntWay =>
-              val newNeighbours = createNeighbour(way.startNode, way.endNode, createNeighbour(way.endNode, way.startNode, neighbours))
-              computeNeighboursRecursive(tail, newNeighbours)
-          }
-      }
-    }
-    info("Computing neighbours")
-    val (time, neighbours) = TimeHelpers.calcTime(computeNeighboursRecursive(ways.values.toList, Map.empty))
-    info("Neighbours computed in %d ms".format(time))
-    neighbours
-  }
-
-  /**
-   * Berechnet alle Knoten, die von einem Knoten erreichbar sind.
-   */
-  def computeReachableNodes = {
-    info("Computing reachable nodes")
-    val (time, reachableNodes) = TimeHelpers.calcTime({
-      val reachableNodes = MutableMap.empty[AntNode, Set[AntNode]]
-      neighbours.map(kv => reachableNodes += (kv._1 -> kv._2))
-      // über alle Knoten iterieren
-      nodes.values.foreach(node => {
-        // Update-Kandidaten ermitteln
-        // es sollen alle Knotenmengen erweitert werden, die vom aktuellen Knoten erreichbar sind
-        val updateCandidates = reachableNodes.par.filter(_._2.contains(node))
-        // über alle Update-Kandidaten iterieren
-        updateCandidates.foreach(updateCandidate => {
-          // erreichbare Knoten setzen sich aus den aktuell erreichbaren Knoten des Update-Kandidaten und den erreichbaren Knoten des Knoten zusammen
-          // der Quell-Knoten wird herausgefiltert, um Kreise zu vermeiden
-          val newReachableNodes = (reachableNodes(node) ++ updateCandidate._2).par.filter(_ != updateCandidate._1).seq
-          reachableNodes += updateCandidate._1 -> newReachableNodes
-        })
-      })
-      reachableNodes.toMap
-    })
-    info("Reachable nodes computed in %d ms".format(time))
-    reachableNodes
   }
 }
 
@@ -151,7 +89,7 @@ object AntMap extends Logger {
             case antWay: AntWay =>
               createAntWays(Nil, Nil, Seq(antWay) ++ antWays)
           }
-        case (_,  Seq(head, tail @ _*)) =>
+        case (_, Seq(head, tail @ _*)) =>
           if (antNodes.contains(head.id)) {
             AntWay(osmWay, antWays.size + 1, (Seq(head) ++ antWayNodes).reverse, antNodes) match {
               case None =>
