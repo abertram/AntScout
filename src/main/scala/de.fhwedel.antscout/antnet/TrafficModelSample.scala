b80ee7e5a9a61384dc84197f14ae87c6b3611125
abertram <bertram.alex@gmail.com>
2012-07-06 08:23:28 +0200

    Berechnung der Verstärkung so modifiziert, dass die Verstärkung am Ende auf jeden Fall > 0 und < 1ist.

--- src/main/scala/de.fhwedel.antscout/antnet/TrafficModelSample.scala
+++ src/main/scala/de.fhwedel.antscout/antnet/TrafficModelSample.scala
@@ -4,13 +4,6 @@ package antnet
 import collection.mutable.Buffer
 import extensions.ExtendedDouble._
 
-/**
- * Created by IntelliJ IDEA.
- * User: alex
- * Date: 06.01.12
- * Time: 12:05
- */
-
 class TrafficModelSample(varsigma: Double, windowSize: Int) {
 
   private var mean = 0.0
@@ -26,8 +19,23 @@ class TrafficModelSample(varsigma: Double, windowSize: Int) {
     variance += varsigma * (math.pow(tripTime - mean, 2) - variance)
   }
 
+  /**
+   * Berechnet die beste (kleinste) Fahrzeit.
+   *
+   * @return Die beste Fahrzeit.
+   */
   def bestTripTime = _tripTimes min
 
+  /**
+   * Berechnet die Verstärkung, die für die Pheromon-Aktualisierung genutzt wird. Am Ende wird "künstlich" dafür
+   * gesorgt, dass die Verstärkung zwischen (exklusive) 0 und 1 liegt. Eine Verstärkung = 0 oder = 1 würde dafür
+   * sorgen, dass einzelne Pheromone = 0 werden. Das würde wiederum dafür sorgen, dass die entsprechenden
+   * Wahrscheinlichkeiten = 0 werden.
+   *
+   * @param tripTime Aktuelle Fahrzeit
+   * @param neighbourCount Anzahl der Nachbarn, die vom aktuellen Knoten erreicht werden können.
+   * @return Errechnete Verstärkung
+   */
   def reinforcement(tripTime: Double, neighbourCount: Double) = {
     val c1 = 0.7
     val c2 = 0.3
@@ -36,7 +44,12 @@ class TrafficModelSample(varsigma: Double, windowSize: Int) {
     val iSup = mean + z * (math.sqrt(variance) / math.sqrt(windowSize))
     val stabilityTerm = (iSup - iInf) + (tripTime - iInf)
     val r = c1 * (bestTripTime / tripTime) + c2 * (if (stabilityTerm ~> 0.0) ((iSup - iInf) / stabilityTerm) else 0)
-    TrafficModelSample.transformBySquash(math.max(DefaultEpsilon, math.min(r, 1)), neighbourCount)
+    // TODO Prüfen, ob die Werte <= 0 und > 1 durch Rechenfehler zustande kommen
+    val squashedReinforcement = TrafficModelSample.transformBySquash(math.max(0.05, math.min(r, 0.95)),
+      neighbourCount)
+    assert(squashedReinforcement ~> 0 && (squashedReinforcement ~< 1 || (neighbourCount == 1 && (squashedReinforcement
+      ~= 1))))
+    squashedReinforcement
   }
 
   def tripTimes = _tripTimes
@@ -47,7 +60,8 @@ object TrafficModelSample {
   def apply(varsigma: Double, windowSize: Int) = new TrafficModelSample(varsigma, windowSize)
 
   /**
-   * Laut Literatur eigentlich (1 + ...)^(-1). Wenn aber die Transformation nicht mit s(r) / s(1) sondern mit s(1) / s(r) aufgerufen wird, kann (...)^(-1) weggelassen werden.
+   * Laut Literatur eigentlich (1 + ...)^(-1). Wenn aber die Transformation nicht mit s(r) / s(1) sondern mit s(1) /
+   * s(r) aufgerufen wird, kann (...)^(-1) weggelassen werden.
    *
    * @param x
    * @param neighbourCount
@@ -67,5 +81,5 @@ object TrafficModelSample {
    * @param a
    * @return
    */
-  def transformBySquash(x: Double, N: Double, a: Double = 100) = squash(1, N, a) / squash(x, N, a)
+  def transformBySquash(x: Double, N: Double, a: Double = 10) = squash(1, N, a) / squash(x, N, a)
 }
