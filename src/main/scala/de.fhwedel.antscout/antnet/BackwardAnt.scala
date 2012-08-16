a4d54b7faffc443ec5d0328aa29f089f3b143f46
abertram <bertram.alex@gmail.com>
2012-08-17 01:11:05 +0200

    Bei der Aktualisierung der Knoten-Daten-Strukturen wurde die Reisezeit Ziel zu den Zwischen-Knoten nicht aufsummiert.

--- src/main/scala/de.fhwedel.antscout/antnet/BackwardAnt.scala
+++ src/main/scala/de.fhwedel.antscout/antnet/BackwardAnt.scala
@@ -2,18 +2,28 @@ package de.fhwedel.antscout
 package antnet
 
 import akka.actor.ActorRef
+import net.liftweb.common.Logger
 
-/**
- * Created by IntelliJ IDEA.
- * User: alex
- * Date: 05.01.12
- * Time: 18:55
- */
+class BackwardAnt(source: ActorRef, destination: ActorRef, memory: AntMemory) extends Logger {
 
-class BackwardAnt(source: ActorRef, destination: ActorRef, memory: AntMemory) {
+  memory.items.foldLeft(0.0) {
+    case (tripTimeAcc, antMemoryItem @ AntMemoryItem(node, way, tripTime)) => {
+      val tripTimeSum = tripTimeAcc + tripTime
+      val updateDataStructures = AntNode.UpdateDataStructures(destination, way, tripTimeSum)
+      trace("Updating data structures: %s" format updateDataStructures)
+      node ! updateDataStructures
+      tripTimeSum
+    }
+  }
 
-  memory.items.foreach {
-    case AntMemoryItem(node, way, tripTime) => node ! AntNode.UpdateDataStructures(destination, way, tripTime)
+  def trace(message: String) {
+    for {
+      traceSourceId <- AntScout.traceSourceId
+      traceDestinationId <- AntScout.traceDestinationId
+      if (source.path.elements.last.matches(traceSourceId) && destination.path.elements.last.matches
+        (traceDestinationId))
+    } yield
+      debug("%s" format message)
   }
 }
 
