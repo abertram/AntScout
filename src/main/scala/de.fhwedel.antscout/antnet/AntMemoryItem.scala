810437078298826efd457ece0bbf57819e0f6082
abertram <bertram.alex@gmail.com>
2012-08-24 17:06:11 +0200

    AntMemoryItem in eine case-Klasse umgewandelt.

--- src/main/scala/de.fhwedel.antscout/antnet/AntMemoryItem.scala
+++ src/main/scala/de.fhwedel.antscout/antnet/AntMemoryItem.scala
@@ -3,14 +3,4 @@ package antnet
 
 import akka.actor.ActorRef
 
-case class AntMemoryItem(node: ActorRef, way: AntWay, tripTime: Double) {
-
-  override def equals(that: Any) = {
-    that match {
-      case ami: AntMemoryItem => node == ami.node && way == ami.way && tripTime == ami.tripTime
-      case _ => false
-    }
-  }
-
-  override def toString = "(Node: %s, way: %s, trip time: %f)".format(node, way id, tripTime)
-}
+case class AntMemoryItem(node: ActorRef, way: AntWay, tripTime: Double)
