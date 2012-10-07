ad3d470fc4621c2057f1f741ca0c8ccb1d59f3c1
abertram <bertram.alex@gmail.com>
2012-10-07 07:40:47 +0200

    Karte "Wedel.osm", Anzahl der Knoten, Quellen und Ziele als Kommentar hinzugefügt. Koordinaten für "Altona-50-Knoten .osm" angepasst.

--- project/Build.scala
+++ project/Build.scala
@@ -8,12 +8,22 @@ object AntScoutBuild extends Build {
   val mapName = "hamburg.osm"
   val mapExtension = ".pbf"
   val originalMap = file("%s/%s%s" format (mapDirectory, mapName, mapExtension))
+  /**
+   * Karten, die erzeugt werden sollen.
+   *
+   * Die Sequenz besteht aus einzelnen Tupeln. Die Tupeln selbst bestehen aus drei Elementen:
+   *
+   * 1. Original-Karte, aus der die einzelnen Karten erzeugt werden.
+   * 2. Koordinaten für den Ausschnitt aus der Original-Karte.
+   * 3. Speicherort und Dateiname der erzeugten Karte.
+   */
   val preprocessedMaps = Seq(
     (originalMap, (9.886, 53.587, 9.93, 53.545), file("%s/%s" format (mapDirectory,
       "Bahrenfeld-Gross-Flottbek-Othmarschen-Ottensen.osm"))),
-    (originalMap, (9.8898, 53.5560023, 9.949, 53.5438), file("%s/%s" format (mapDirectory, "Altona-50-Knoten.osm"))),
+    (originalMap, (9.926, 53.5518, 9.9559, 53.54), file("%s/%s" format (mapDirectory, "Altona-50-Knoten.osm"))),
     (originalMap, (9.9362, 53.55131, 9.949, 53.5468), file("%s/%s" format (mapDirectory, "Altona-Kreis.osm"))),
-    (originalMap, (9.69, 53.5856, 9.892, 53.5464), file("%s/%s" format (mapDirectory, "Altona-Wedel.osm")))
+    (originalMap, (9.69, 53.5856, 9.892, 53.5464), file("%s/%s" format (mapDirectory, "Altona-Wedel.osm"))),
+    (originalMap, (9.693, 53.5878, 9.7395, 53.5642), file("%s/%s" format (mapDirectory, "Wedel.osm")))
   )
 
   val downloadMap = TaskKey[Unit]("download-map")
